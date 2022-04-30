2a79d2926ae1d5973272981ed792e247de1a9715
awyxx <robyawyx@gmail.com>
2022-04-30 02:29:47 +0100

    added rust similar types

---
+++ src/types.hpp
@@ -0,0 +1,21 @@
+#ifndef TYPES_HPP
+#define TYPES_HPP
+
+#include <iostream>
+#include <stdint.h>
+
+// xd.
+
+using usize = std::size_t;
+
+using s8  = int8_t;
+using s16 = int16_t;
+using s32 = int32_t;
+using s64 = int64_t;
+
+using u8  = uint8_t;
+using u16 = uint16_t;
+using u32 = uint32_t;
+using u64 = uint64_t;
+
+#endif
