9d481d0e2735e8a2a6f9578681225eb75b2ba224
roby2014 <robyawyx@gmail.com>
2022-05-20 00:23:26 +0100

    gui_debugger class implementation, on main.cpp for now

---
+++ src/gui_debugger.hpp
@@ -0,0 +1,15 @@
+#ifndef GUI_DEBUGGER_HPP
+#define GUI_DEBUGGER_HPP
+
+#ifdef _WIN32
+#include <Windows.h>
+#include <commdlg.h>
+#endif
+
+#include <iostream>
+#include "gui_debugger.hpp"
+#include "chip8.hpp"
+#include "imgui.h"
+#include "imgui-SFML.h"
+#include <SFML/Graphics.hpp>
+
--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,8 @@
 #include <iostream>
+//#include "gui_debugger.hpp"
+#include "chip8.hpp"
 #include "imgui.h"
 #include "imgui-SFML.h"
-#include "chip8.hpp"
-
 #include <SFML/Graphics.hpp>
 
 #ifdef _WIN32
@@ -14,72 +14,87 @@
 #define WINDOW_HEIGHT 320
 #define SCALE_FACTOR  10 // 64*10 = 640 ; 32*10 = 340 ..
 
-void code_dock() {
-    ImGui::Begin("Code");
+class gui_debugger {
+public:
+    bool _DEBUG_MODE;
 
-    static char text[1024 * 16] =
-        "/*\n"
-        " int main() {\n"
-        "   return 0;\n"
-        " }\n"
-        "label:\n"
-        "\tlock cmpxchg8b eax\n";
-
-    static ImGuiInputTextFlags flags = ImGuiInputTextFlags_AllowTabInput;
-    ImGui::CheckboxFlags("ImGuiInputTextFlags_ReadOnly", &flags, ImGuiInputTextFlags_ReadOnly);
-    ImGui::CheckboxFlags("ImGuiInputTextFlags_AllowTabInput", &flags, ImGuiInputTextFlags_AllowTabInput);
-    ImGui::InputTextMultiline("##source", text, IM_ARRAYSIZE(text), ImVec2(-FLT_MIN, ImGui::GetTextLineHeight() * 16), flags);
-
-    ImGui::End();
-}
-
-bool DEBUG_MODE = false;
-
-void show_main_menu_bar(sf::RenderWindow* window, chip8* emu, bool* rom_loaded) {
-    if (ImGui::BeginMainMenuBar()) {
-        if (ImGui::BeginMenu("File")) {
-            if (ImGui::MenuItem("Load ROM", "Ctrl+O")) {
-                // if LINUX
-                char fname[1024];
-#ifdef linux
-                FILE* fp = popen("zenity --file-selection", "r");
-                fgets(fname, sizeof(fname), fp);
-                if (fname[strlen(fname) - 1] == '\n') {
-                    fname[strlen(fname) - 1] = 0;
-                }
-#elif _WIN32
-                OPENFILENAMEA f;
-                f.lStructSize = sizeof(f);
-                f.hwndOwner = GetActiveWindow();
-                f.lpstrFile = fname;
-                f.nMaxFile = sizeof(fname);
-                if (!GetOpenFileNameA(&f)) {
-                    return;
-                }
-#endif
-                emu->load_rom(fname);
-                *rom_loaded = true;
-            }
-            ImGui::EndMenu();
-        }
-        else if (ImGui::MenuItem("Exit")) {
-            window->close();
-        }
-        ImGui::EndMainMenuBar();
+    void registers_dock() {
+        ImGui::Begin("Registers");
+        ImGui::End();
     }
-}
+
+    void memory_dock() {
+        ImGui::Begin("Memory");
+        ImGui::End();
+    }
+
+    void code_dock() {
+        ImGui::Begin("Code");
+
+        static char text[1024 * 16] =
+            "/*\n"
+            " int main() {\n"
+            "   return 0;\n"
+            " }\n"
+            "label:\n"
+            "\tlock cmpxchg8b eax\n";
+
+        static ImGuiInputTextFlags flags = ImGuiInputTextFlags_AllowTabInput;
+        ImGui::CheckboxFlags("ImGuiInputTextFlags_ReadOnly", &flags, ImGuiInputTextFlags_ReadOnly);
+        ImGui::CheckboxFlags("ImGuiInputTextFlags_AllowTabInput", &flags, ImGuiInputTextFlags_AllowTabInput);
+        ImGui::InputTextMultiline("##source", text, IM_ARRAYSIZE(text), ImVec2(-FLT_MIN, ImGui::GetTextLineHeight() * 16), flags);
+
+        ImGui::End();
+    }
+
+    void show_main_menu_bar(sf::RenderWindow* window, chip8* emu, bool* rom_loaded) {
+        if (ImGui::BeginMainMenuBar()) {
+            if (ImGui::BeginMenu("File")) {
+                if (ImGui::MenuItem("Load ROM", "Ctrl+O")) {
+                    // if LINUX
+                    char fname[1024];
+#ifdef linux
+                    FILE* fp = popen("zenity --file-selection", "r");
+                    fgets(fname, sizeof(fname), fp);
+                    if (fname[strlen(fname) - 1] == '\n') {
+                        fname[strlen(fname) - 1] = 0;
+                    }
+#elif _WIN32
+                    // FIXME
+                    OPENFILENAMEA f;
+                    f.lStructSize = sizeof(f);
+                    f.hwndOwner = GetActiveWindow();
+                    f.lpstrFile = fname;
+                    f.nMaxFile = sizeof(fname);
+                    if (!GetOpenFileNameA(&f)) {
+                        return;
+                    }
+#endif
+                    emu->load_rom(fname);
+                    *rom_loaded = true;
+                }
+                ImGui::EndMenu();
+            }
+            else if (ImGui::MenuItem("Exit")) {
+                window->close();
+            }
+
+            else if (ImGui::MenuItem(_DEBUG_MODE ? "Normal mode" : "Debug Mode")) {
+                _DEBUG_MODE = !_DEBUG_MODE;
+            }
+            ImGui::EndMainMenuBar();
+        }
+    }
+};
 
 int main(int argc, char** argv) {
+    gui_debugger gui_dbg;
+
     if (argc == 2) {
-        DEBUG_MODE = argv[1][0] == '0' ? false : true;
-    }
-    else {
-        DEBUG_MODE = false;
+        gui_dbg._DEBUG_MODE = argv[1][0] == '0' ? false : true;
     }
 
     chip8 emu;
-
-    std::cout << "DEBUG MODE IS SET TO " << DEBUG_MODE;
 
     sf::RenderWindow window(sf::VideoMode(1200, 600), "CHIP-8 Emulator");
     window.setFramerateLimit(60);
@@ -99,12 +114,14 @@ int main(int argc, char** argv) {
         }
 
         auto& io = ImGui::GetIO();
-        if (!DEBUG_MODE) io.ConfigFlags |= ImGuiConfigFlags_DockingEnable;
+        if (!gui_dbg._DEBUG_MODE) {
+            io.ConfigFlags |= ImGuiConfigFlags_DockingEnable;
+        }
         ImGui::SFML::Update(window, deltaClock.restart());
 
-        show_main_menu_bar(&window, &emu, &rom_loaded);
+        gui_dbg.show_main_menu_bar(&window, &emu, &rom_loaded);
 
-        if (!DEBUG_MODE) {
+        if (!gui_dbg._DEBUG_MODE) {
             //ImGui::ShowDemoWindow();
             if (rom_loaded) {
                 ImGui::Text("Rom is loaded");
@@ -119,7 +136,6 @@ int main(int argc, char** argv) {
                     pixel.setFillColor(emu.get_pixel(idx) == 1 ? sf::Color::White : sf::Color::Black);
                     //window.draw(pixel);
 
-
                     if ((idx + 1) % CHIP8_WIDTH == 0) {
                         row++;
                     }
@@ -130,13 +146,9 @@ int main(int argc, char** argv) {
         else {
             ImGui::DockSpaceOverViewport(ImGui::GetMainViewport());
 
-            code_dock();
-
-            ImGui::Begin("Registers");
-            ImGui::End();
-
-            ImGui::Begin("Memory");
-            ImGui::End();
+            gui_dbg.code_dock();
+            gui_dbg.memory_dock();
+            gui_dbg.registers_dock();
         }
 
 
